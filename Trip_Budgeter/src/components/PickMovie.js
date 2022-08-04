b2ad5b698a41f6dd32c43c9b51ac1c87370d2e1b
Yicheng Huang <huanyic1@gmail.com>
2022-08-04 17:05:49 -0400

    rand

--- Trip_Budgeter/src/components/PickMovie.js
+++ Trip_Budgeter/src/components/PickMovie.js
@@ -22,7 +22,7 @@ var CityVal;
 const PickMovie = () => {
     const [randomMovie, setRandomMovie] = useState({});
     const [picked, setPicked] = useState(false)
-<<<<<<< HEAD
+
     const [city, setCity] = useState('');
 
     const cityHandler = (event) => {
@@ -31,9 +31,7 @@ const PickMovie = () => {
         console.log(event.target.value);
     }
 
-=======
-    
->>>>>>> 8f6bb71dae5cd21875b508bff5258991213d62e3
+
     const  moviePicker = () => {
         const city = obj => obj.value === CityVal;
         console.log(CityVal);
@@ -54,41 +52,7 @@ const PickMovie = () => {
     const pickAgain = () => {
         setPicked(false)
     }
-<<<<<<< HEAD
-    return (
 
-        <div>
-            <h1 className="title">Let's Travel!</h1>
-            <label>Location</label>
-            <div>
-                <select onChange={cityHandler}>
-                    <option value="None">Select</option>
-                    <option value="NYC">New York City</option>
-                    <option value="BOS">Boston</option>
-                </select>
-            </div>
-            <div>
-            <label>Number of Days Staying</label>
-            </div>
-            <div>
-            <input></input>
-            </div>
-            
-            {picked && 
-            <div className ="picked">
-                <div></div>
-                <Movie name={randomMovie.name} image={randomMovie.img} genre={randomMovie.genre}/>
-                <button className="pickButton" onClick={pickAgain} >Pick Again</button>
-            </div>}
-            {!picked && <button onClick={moviePicker}>Let's Go!</button>}
-            
-            
-        </div>
-    )
-=======
-
-    
-    
     
         return (
            <div>
@@ -123,9 +87,7 @@ const PickMovie = () => {
        </div>
 
         );
-    
-    
->>>>>>> 8f6bb71dae5cd21875b508bff5258991213d62e3
+
 }
 
 export default PickMovie
