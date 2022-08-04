cb90e40e6c0af8e1d2cc78bb38cde42838bcdf94
Isaac <ikenxtgen@gmail.com>
2022-08-04 14:43:19 -0400

    changes to console log and identify the changes in dropdown menu for addmovie.js

--- Trip_Budgeter/src/data/citydata.js
+++ Trip_Budgeter/src/data/citydata.js
@@ -1,11 +1,12 @@
-export const citydata = [
+export let citydata = [
     {
         city: 'New York City',
         state: 'New York',
-        avghotelcost: 500,
+        living: 500,
         food: 25,
         entertainment: 20,
         transportation: 15
 
     },
 ]
+
