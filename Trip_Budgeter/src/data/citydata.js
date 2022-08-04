8f6bb71dae5cd21875b508bff5258991213d62e3
Isaac <ikenxtgen@gmail.com>
2022-08-04 15:11:37 -0400

    Changes to the city data file to add data for boston

--- Trip_Budgeter/src/data/citydata.js
+++ Trip_Budgeter/src/data/citydata.js
@@ -2,8 +2,19 @@ export let citydata = [
     {
         city: 'New York City',
         state: 'New York',
+        value: 'NYC',
         living: 500,
         food: 25,
+        entertainment: 20,
+        transportation: 15
+
+    }, 
+    {
+        city: 'Boston',
+        state: 'Massachusetts',
+        value: 'BOS',
+        living: 400,
+        food: 20,
         entertainment: 20,
         transportation: 15
 
