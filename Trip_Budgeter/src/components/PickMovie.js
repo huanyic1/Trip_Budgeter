6b83b86f6a8cc08428cd3c80b13d156e22afd4da
Jonathan Serrano <jonathan.ant.serrano@gmail.com>
2022-08-04 14:12:19 -0500

    Be able to choose correct city

--- Trip_Budgeter/src/components/PickMovie.js
+++ Trip_Budgeter/src/components/PickMovie.js
@@ -3,14 +3,25 @@ import Movie from './Movie';
 import {data} from '../data/data'
 import './PickMovie.css'
 
+var CityVal;
+
+
 const PickMovie = () => {
     const [randomMovie, setRandomMovie] = useState({});
     const [picked, setPicked] = useState(false)
+    const [city, setCity] = useState('');
+
+    const cityHandler = (event) => {
+        setCity(event.target.value);
+        CityVal = event.target.value;
+        console.log(event.target.value);
+    }
 
     const  moviePicker = () => {
-        const random = data[Math.floor(Math.random() * data.length)];
-        console.log(randomMovie)
-        setRandomMovie(random);
+        const city = obj => obj.value === CityVal;
+        console.log(CityVal);
+        //console.log(randomMovie)
+        setRandomMovie(data[data.findIndex(city)]);
         setPicked(true);
     }
 
@@ -23,7 +34,7 @@ const PickMovie = () => {
             <h1 className="title">Let's Travel!</h1>
             <label>Location</label>
             <div>
-                <select>
+                <select onChange={cityHandler}>
                     <option value="None">Select</option>
                     <option value="NYC">New York City</option>
                     <option value="BOS">Boston</option>
