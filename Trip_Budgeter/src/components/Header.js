35eb92e80d18880b6de1d974f81e60a99f10f76e
Jonathan Serrano <jonathan.ant.serrano@gmail.com>
2022-08-04 16:14:38 -0500

    Add Logo that links to home page

--- Trip_Budgeter/src/components/Header.js
+++ Trip_Budgeter/src/components/Header.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import './Header.css'
+import logo from './Logo2.png';
 
 const Header = () => {
     return (
         <div className="header">
+            <Link to= "/">
+             <img src={logo} className="icon"></img>
+             </Link>
             <h1 className="logo"> Trip Budgeter</h1>
             <div className="links">
                 <Link to="/trip">Take a trip!</Link>
