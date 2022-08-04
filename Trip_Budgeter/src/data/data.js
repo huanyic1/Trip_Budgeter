8bc06e4842f5f602320e7f4b6ef358707cbbc43b
Isaac <ikenxtgen@gmail.com>
2022-08-04 10:33:40 -0400

    main page additions

--- Trip_Budgeter/src/data/data.js
+++ Trip_Budgeter/src/data/data.js
@@ -1,18 +1,18 @@
 export const data = [
     {
-        img: "https://m.media-amazon.com/images/M/MV5BN2FjNmEyNWMtYzM0ZS00NjIyLTg5YzYtYThlMGVjNzE1OGViXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_FMjpg_UX1000_.jpg",
-        name: "Dune",
-        genre: "Science Fiction"
+        img: "https://images.unsplash.com/photo-1538970272646-f61fabb3a8a2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8bmV3JTIweW9yayUyMGNpdHl8ZW58MHx8MHx8&w=1000&q=80://images.unsplash.com/photo-1448317846460-907988886b33?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MTF8MTY2MzE0fHxlbnwwfHx8fA%3D%3D&w=1000&q=80",
+        name: "New York City",
+        genre: "New York"
     },
     {
-        img: "https://m.media-amazon.com/images/M/MV5BOTY2NzFjODctOWUzMC00MGZhLTlhNjMtM2Y2ODBiNGY1ZWRiXkEyXkFqcGdeQXVyMDM2NDM2MQ@@._V1_.jpg",
-        name: "Free Guy",
-        genre: "Comedy"
+        img: "https://images.unsplash.com/photo-1514924013411-cbf25faa35bb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=690&q=80",
+        name: "Boston",
+        genre: "Massachusetts"
     },
     {
-        img: "https://terrigen-cdn-dev.marvel.com/content/prod/1x/spider-mannowayhome_lob_crd_03.jpg",
-        name: "Spider Man: No Way Home",
-        genre: "Action"
+        img: "https://images.unsplash.com/photo-1609924211018-5526c55bad5b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+        name: "Los Angeles",
+        genre: "California"
     }
 
 ]
