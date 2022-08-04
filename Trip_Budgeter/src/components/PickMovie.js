a7d5ee65f7f7a46f5827d153f71b467b8d08c7cf
Yicheng Huang <huanyic1@gmail.com>
2022-08-04 14:33:41 -0400

    added pie chart

--- Trip_Budgeter/src/components/PickMovie.js
+++ Trip_Budgeter/src/components/PickMovie.js
@@ -1,12 +1,25 @@
 import React, {useState} from 'react'
 import Movie from './Movie';
+import Trip from "./Trip"; 
 import {data} from '../data/data'
 import './PickMovie.css'
+import { PieChart,
+    Pie,
+    Tooltip,
+    BarChart,
+    XAxis,
+    YAxis,
+    Legend,
+    CartesianGrid,
+    ResponsiveContainer,
+    Bar} from "recharts"; 
+
+
 
 const PickMovie = () => {
     const [randomMovie, setRandomMovie] = useState({});
     const [picked, setPicked] = useState(false)
-
+    
     const  moviePicker = () => {
         const random = data[Math.floor(Math.random() * data.length)];
         console.log(randomMovie)
@@ -14,39 +27,57 @@ const PickMovie = () => {
         setPicked(true);
     }
 
+
+
+    const displayLocation = () =>{
+        console.log(<Trip/>)
+        setPicked(true)
+        
+    }
+
+   
     const pickAgain = () => {
         setPicked(false)
     }
-    return (
 
-        <div>
-            <h1 className="title">Let's Travel!</h1>
-            <label>Location</label>
-            <div>
-                <select>
-                    <option value="None">Select</option>
-                    <option value="NYC">New York City</option>
-                    <option value="BOS">Boston</option>
-                </select>
-            </div>
-            <div>
-            <label>Number of Days Staying</label>
-            </div>
-            <div>
-            <input></input>
-            </div>
-            
-            {picked && 
-            <div className ="picked">
-                <div></div>
-                <Movie name={randomMovie.name} image={randomMovie.img} genre={randomMovie.genre}/>
-                <button className="pickButton" onClick={pickAgain} >Pick Again</button>
-            </div>}
-            {!picked && <button onClick={moviePicker}>Let's Go!</button>}
-            
-            
-        </div>
-    )
+    
+    
+    
+        return (
+           <div>
+           <h1 className="title">Let's Travel!</h1>
+           <label>Location</label>
+           <div>
+               <select>
+                   <option value="None">Select</option>
+                   <option value="NYC">New York City</option>
+                   <option value="BOS">Boston</option>
+               </select>
+           </div>
+           <div>
+           <label>Number of Days Staying</label>
+           </div>
+           <div>
+           <input></input>
+           </div>
+           
+           {picked && 
+           <div className ="picked">
+               <div>
+               <Trip/>
+               </div>
+               <div><button className="pickButton" onClick={pickAgain} >Travel to Different Location</button> </div>
+
+               
+           </div>}
+           {!picked && <button onClick={displayLocation}>Let's Go!</button>}
+           
+           
+       </div>
+
+        );
+    
+    
 }
 
 export default PickMovie
