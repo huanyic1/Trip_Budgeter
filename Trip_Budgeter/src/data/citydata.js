24189136b85c2ae5dad2fbec81388cd397ef71e3
Isaac <ikenxtgen@gmail.com>
2022-08-04 10:55:58 -0400

    city data for new york updated

---
+++ Trip_Budgeter/src/data/citydata.js
@@ -0,0 +1,11 @@
+export const citydata = [
+    {
+        city: 'New York City',
+        state: 'New York',
+        avghotelcost: 500,
+        food: 25,
+        entertainment: 20,
+        transportation: 15
+
+    },
+]
