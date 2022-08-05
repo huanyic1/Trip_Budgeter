1689e54da68a1f68ea4a0100f21a2f0d4da53f42
Jonathan Serrano <jonathan.ant.serrano@gmail.com>
2022-08-04 19:11:39 -0500

    Added Styling to Homepage

--- Trip_Budgeter/src/components/Header.js
+++ Trip_Budgeter/src/components/Header.js
@@ -1,15 +1,17 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import './Header.css'
-import logo from './Logo2.png';
+import logo from './Logo3.png';
+
 
 const Header = () => {
     return (
         <div className="header">
             <Link to= "/">
              <img src={logo} className="icon"></img>
+             <h1 className="logo"> Capital Voyagers</h1>
              </Link>
-            <h1 className="logo"> Trip Budgeter</h1>
+             {/* <h1 className="logo"> Capital Voyagers</h1> */}
             <div className="links">
                 <Link to="/trip">Take a trip!</Link>
                 <Link to="/add">Help us improve!</Link>
